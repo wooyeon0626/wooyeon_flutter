51c755bd2fcf0ada557c9438f2fbd673076f0840
Haneol <haneolj@gmail.com>
2023-08-12 13:21:13 +0900

    [ETC] Fix login request parameters

--- lib/service/login/phone_auth.dart
+++ lib/service/login/phone_auth.dart
@@ -7,16 +7,24 @@ import 'package:http/http.dart' as http;
 class PhoneAuth {
   /// send phone number to server.
   /// then, the app will receive text message with a verification code to that phone number.
-  Future<void> sendPhoneNumberRequest({required String phone}) async {
+  Future<bool> sendPhoneNumberRequest({required String phone}) async {
     const url = '${Config.domain}/auth/phone';
 
-    await http.post(Uri.parse(url), headers: <String, String>{
+    final response = await http.post(Uri.parse(url), headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
       body: jsonEncode(<String, String>{
-        'phone': phone,
+        'to': phone,
       }),
     );
+
+    if(response.statusCode == 200) {
+      return true;
+    } else {
+      log("Error with status code : ${response.statusCode}");
+      return false;
+    }
+
   }
 
   /// send phone number and code to server.
