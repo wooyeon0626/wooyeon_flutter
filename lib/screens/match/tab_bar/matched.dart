0dc20542e14fe88d21ee0521176c9ba31a7471ee
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-10-04 17:58:52 +0900

    [FEAT] Matched list UI added.

--- lib/screens/match/tab_bar/matched.dart
+++ lib/screens/match/tab_bar/matched.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wooyeon_flutter/models/controller/chat_controller.dart';
+import 'package:wooyeon_flutter/models/data/chat_room_data.dart';
+import 'package:wooyeon_flutter/widgets/chat/chatroom_listview.dart';
 
 class Matched extends StatelessWidget {
   const Matched({super.key});
 
+  RxMap<int, ChatRoom> filterNoChatRooms(RxMap<int, ChatRoom> chatRooms){
+
+    RxMap<int, ChatRoom> result;
+
+    for(int i=0;i<chatRooms.length;i++){
+      if(chatRooms[i].obs.value?.chat == null){
+
+      }
+    }
+
+    return chatRooms;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("매치된 친구"),);
+    return GetBuilder<ChatController>(
+      init: Get.find<ChatController>(),
+      builder: (controller) => Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+        child: ChatRoomListView(filterNoChatRooms(controller.chatRooms)),
+      ),
+    );
   }
 
 }
