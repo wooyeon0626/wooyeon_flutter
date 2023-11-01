a8a49af50b5db1e749b9c1b405b81f2e4da04619
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-11-01 12:08:36 +0900

    [FIX] delete frequent from ChatRoom class and chatRoomData

--- lib/models/data/chat_room_data.dart
+++ lib/models/data/chat_room_data.dart
@@ -6,25 +6,23 @@ class ChatRoom {
   bool isOnline;
   MatchProfiles matched;
   List<ChatData>? chat;
-  int frequent;
 
   ChatRoom(
       {required this.chatRoomId,
       required this.isOnline,
       required this.matched,
-      this.chat,
-      required this.frequent});
+      this.chat,});
 }
 
 List<ChatRoom> chatRoomData = [
-  ChatRoom(chatRoomId: 0, isOnline: true, matched: matchProfiles[0], chat: chatData, frequent: 0),
-  ChatRoom(chatRoomId: 1, isOnline: false, matched: matchProfiles[1], chat: chatData2, frequent: 2),
-  ChatRoom(chatRoomId: 2, isOnline: true, matched: matchProfiles[2], chat: chatData3, frequent: 4),
-  ChatRoom(chatRoomId: 3, isOnline: false, matched: matchProfiles[3], chat: chatData4, frequent: 1),
-  ChatRoom(chatRoomId: 4, isOnline: true, matched: matchProfiles[4], chat: null, frequent: 3),
-  ChatRoom(chatRoomId: 5, isOnline: true, matched: matchProfiles[0], chat: null, frequent: 5),
-  ChatRoom(chatRoomId: 6, isOnline: false, matched: matchProfiles[1], chat: null, frequent: 6),
-  ChatRoom(chatRoomId: 7, isOnline: true, matched: matchProfiles[2], chat: null, frequent: 7),
-  ChatRoom(chatRoomId: 8, isOnline: false, matched: matchProfiles[3], chat: null, frequent: 8),
-  ChatRoom(chatRoomId: 9, isOnline: true, matched: matchProfiles[4], chat: null, frequent: 9),
+  ChatRoom(chatRoomId: 0, isOnline: true, matched: matchProfiles[0], chat: chatData),
+  ChatRoom(chatRoomId: 1, isOnline: false, matched: matchProfiles[1], chat: chatData2),
+  ChatRoom(chatRoomId: 2, isOnline: true, matched: matchProfiles[2], chat: chatData3),
+  ChatRoom(chatRoomId: 3, isOnline: false, matched: matchProfiles[3], chat: chatData4),
+  ChatRoom(chatRoomId: 4, isOnline: true, matched: matchProfiles[4], chat: null),
+  ChatRoom(chatRoomId: 5, isOnline: true, matched: matchProfiles[0], chat: null),
+  ChatRoom(chatRoomId: 6, isOnline: false, matched: matchProfiles[1], chat: null),
+  ChatRoom(chatRoomId: 7, isOnline: true, matched: matchProfiles[2], chat: null),
+  ChatRoom(chatRoomId: 8, isOnline: false, matched: matchProfiles[3], chat: null),
+  ChatRoom(chatRoomId: 9, isOnline: true, matched: matchProfiles[4], chat: null),
 ];
