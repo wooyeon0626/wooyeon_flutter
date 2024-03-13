c2526b3f3e0602a1a2d8f5d03c932d5658bb2621
Haneol <haneolj@gmail.com>
2024-03-13 21:53:50 +0900

    [FEAT] Modify Login success page

--- lib/screens/login/login/login_success.dart
+++ lib/screens/login/login/login_success.dart
@@ -1,15 +1,16 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:wooyeon_flutter/models/pref.dart';
 import 'package:wooyeon_flutter/screens/main_screen.dart';
-import 'package:wooyeon_flutter/service/login/auto_login/auth.dart';
 
 import '../../../config/palette.dart';
 import '../../../widgets/next_button.dart';
 
 class LoginSuccess extends StatelessWidget {
-
   LoginSuccess({super.key});
 
+  final nickname = Pref.instance.profileData?.nickname;
   final buttonActive = ValueNotifier<bool>(true);
 
   // todo : 혹시 로그인 최종 과정에서 필요한 작업이 있다면 여기에서 코드 추가 작성
@@ -21,6 +22,17 @@ class LoginSuccess extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
+        leadingWidth: 80,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            EvaIcons.arrowIosBack,
+            color: Palette.black,
+          ),
+          iconSize: 40,
+        ),
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarColor: Colors.white,
 
@@ -49,16 +61,32 @@ class LoginSuccess extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text(
-                  "근처에 xx명의 친구들이\nxx님을 찾고있어요!",
-                  style: TextStyle(
-                    color: Palette.black,
-                    fontSize: 18,
+              Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: RichText(
+                      text: TextSpan(
+                          style: const TextStyle(
+                            color: Palette.black,
+                            fontSize: 20,
+                          ),
+                          children: [
+                        const TextSpan(text: '근처에 '),
+                        const TextSpan(
+                          text: '137명',
+                          style: TextStyle(
+                              color: Palette.secondary,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const TextSpan(text: '의 친구들이\n'),
+                        TextSpan(
+                          text: '$nickname',
+                          style: const TextStyle(
+                              color: Palette.secondary,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const TextSpan(text: '님을 찾고있어요!\n'),
+                      ]))
                   ),
-                ),
-              ),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
@@ -68,8 +96,10 @@ class LoginSuccess extends StatelessWidget {
                           func: () {
                             Navigator.pushAndRemoveUntil(
                               context,
-                              MaterialPageRoute(builder: (BuildContext context) => const MainScreen()),
-                                  (Route<dynamic> route) => false,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const MainScreen()),
+                              (Route<dynamic> route) => false,
                             );
                           },
                           text: "다음",
